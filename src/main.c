cfccc87e2ea409a215d0656c49449cd17b734a0b
Jero Sutlovic <jsutlovic@gmail.com>
2015-08-26 00:20:41 -0400

    Add world fills for testing

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "world.h"
+#include "game.h"
 
 int main() {
-    world *w = init_world(10, 10);
+    world *w = init_world(100, 100);
     printf("World size: %lu\n", w->data_size);
+    fill1(w);
     print_world(w);
     destroy_world(w);
     return EXIT_SUCCESS;
