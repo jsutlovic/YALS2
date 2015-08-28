221142ca51e02286d862d11223f8741117fce8d4
Jero Sutlovic <jsutlovic@gmail.com>
2015-08-28 09:57:49 -0400

    Start code for benchmarking

--- src/main.c
+++ src/main.c
@@ -4,11 +4,14 @@
 #include "fills.h"
 
 int main() {
-    game *g = init_game(10, 10, conways_life);
+    game *g = init_game(200, 200, conways_life);
     printf("World size: %lu\n", g->w->data_size);
     fill1(g->w);
-    print_world(g->w);
-    game_half_step(g);
+    puts("Start!");
+    for (int i = 0; i < 8000; i++) {
+        game_step(g);
+    }
+    puts("End!");
     destroy_game(g);
     return EXIT_SUCCESS;
 }
