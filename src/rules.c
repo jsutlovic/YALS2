da3491b20e8cba87cb1336ffa29e0b9dedef38ec
Jero Sutlovic <jsutlovic@gmail.com>
2015-08-28 08:56:35 -0400

    Add rule to debug counts, rules return a single bit

--- src/rules.c
+++ src/rules.c
@@ -1,9 +1,15 @@
+#include <stdio.h>
 #include "rules.h"
+
+world_store debug_count_rule(world_store state, world_store sum9) {
+    printf("%d ", sum9);
+    return state & 1;
+}
 
 world_store conways_life(world_store state, world_store sum9) {
     switch(sum9) {
         case 3: return 1;
-        case 4: return state;
+        case 4: return state & 1;
         default: return 0;
     }
 }
--- src/rules.h
+++ src/rules.h
@@ -5,6 +5,7 @@
 
 typedef world_store (*rule_calc_func_type) (world_store, world_store);
 
+world_store debug_count_rule(world_store state, world_store sum9);
 world_store conways_life(world_store state, world_store sum9);
 
 #endif
