57cc5f11d0b0bbc2adbca68c08e851d0e1a56d38
Jero Sutlovic <jsutlovic@gmail.com>
2015-08-30 22:51:47 -0400

    Build script takes CMake build type as argument

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ rm -rf "$PROJECT_PATH/build" > /dev/null
 mkdir "$PROJECT_PATH/build"
 
 pushd "$PROJECT_PATH/build" > /dev/null
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=${1:-Debug} ../
 popd > /dev/null
