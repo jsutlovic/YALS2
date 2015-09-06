476f761fde5a5c49d5fb0c074f68591a5310029b
Jero Sutlovic <jsutlovic@gmail.com>
2015-09-05 22:12:42 -0400

    Document fills

--- src/fills.h
+++ src/fills.h
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 #include "world.h"
 
-void fill0(world *w);
-void fill1(world *w);
-void fill2(world *w);
-void fill3(world *w);
-void fill4(world *w);
-void fill5(world *w);
+void fill0(world *w); // Full fill (all 3s)
+void fill1(world *w); // Every even cell (in-row) is alive
+void fill2(world *w); // Every even cell (in-world) is alive
+void fill3(world *w); // cell_val = 0, 1, 2, 3, 0, 1, ...
+void fill4(world *w); // Every odd cell (in-row) is alive
+void fill5(world *w); // Every odd cell (in-world) is alive
 
 #endif
 /* vim: set ft=c : */
